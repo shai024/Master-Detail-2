517116353058e44c79e4c317089630f281b92e40
shai024 <britney.aiken@gmail.com>
2021-03-04 15:55:10 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-7jx1ew
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-7jx1ew)
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,21 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Dr Nice' },
+  { id: 2, name: 'Narco' },
+  { id: 3, name: 'Bombasto' },
+  { id: 4, name: 'Celeritas' },
+  { id: 5, name: 'Magneta' },
+  { id: 6, name: 'RubberMan' },
+  { id: 7, name: 'Dynama' },
+  { id: 8, name: 'Dr IQ' },
+  { id: 9, name: 'Magma' },
+  { id: 10, name: 'Tornado' }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
